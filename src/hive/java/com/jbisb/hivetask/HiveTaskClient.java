fff5f2a38764cf674c1e68489786a06e9ed118f9
Jay B <jbisbad4@gmail.com>
2026-07-17 20:00:03 +0100

    Disable vine traversal for task bots

--- src/hive/java/com/jbisb/hivetask/HiveTaskClient.java
+++ src/hive/java/com/jbisb/hivetask/HiveTaskClient.java
@@ -1554,9 +1554,6 @@ public final class HiveTaskClient {
 
     private void configureMiningTravelSettings() {
         configureMiningSettings();
-        // Jungle routes may require climbing vines to reach top-down cells. Travel
-        // uses goal-directed progress tracking, so climb/fall loops still time out.
-        BaritoneAPI.getSettings().allowVines.value = true;
         BaritoneAPI.getSettings().allowPlace.value = true;
         BaritoneAPI.getSettings().allowParkourPlace.value = false;
     }
